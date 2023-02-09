73d00b6d43266dd13f5eea3c499413f137fda257
TheFlippedSuit <111720831+TheFlippedSuit@users.noreply.github.com>
2023-02-08 18:34:53 -0600

    Update GripperSubsystem.java

--- src/main/java/frc/robot/subsystems/GripperSubsystem.java
+++ src/main/java/frc/robot/subsystems/GripperSubsystem.java
@@ -2,12 +2,12 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.Counter;
 import edu.wpi.first.wpilibj.motorcontrol.Spark;
-import edu.wpi.first.wpilibj.motorcontrol.Talon;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import java.lang.Math;;
 
 public class GripperSubsystem extends SubsystemBase {
-
+    double counterPower;
     Counter counter;
     Spark gripperTalon;
     public GripperSubsystem() {
@@ -18,12 +18,31 @@ public class GripperSubsystem extends SubsystemBase {
   //counter.setUpSource(4);
 
  // Set the encoder to count pulse duration from rising edge to falling edge
-
+ if (counterPower > 0) {
+    counter.setReverseDirection(false);
+ } else {
+    counter.setReverseDirection(true);
+ }
  
     }
 
     public void grip(double power) {
         gripperTalon.set(power);
         SmartDashboard.putNumber("Counter", counter.get());
+        counterPower = power;
+
+    }
+    public void moveGrip(int counts, char direction, double power) {
+        int tempCounts = counter.get();
+        int countTime = Math.abs(counter.get() - tempCounts);
+        while(countTime>counts){
+            if(direction == 'r') {
+                gripperTalon.set(power);
+            } else if(direction == 'l') {
+                gripperTalon.set(-1 * power);
+            } else {
+                System.out.println("Invalid Gripper Command!");
+            }
+        }
     }
 }
