2d626ce1f4db57eafa016fcb3b8e1d52222a8170
TheFlippedSuit <111720831+TheFlippedSuit@users.noreply.github.com>
2023-02-17 15:46:08 -0600

    STEVEN

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -1,26 +1,64 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.CAN;
 import edu.wpi.first.wpilibj.Counter;
 import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import java.util.NavigableMap;
 
+import org.apache.commons.lang3.function.TriFunction;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
 import com.revrobotics.SparkMaxAbsoluteEncoder;
+import com.revrobotics.SparkMaxPIDController;
+import com.revrobotics.CANSparkMax.ControlType;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
 
 public class ArmSubsystem extends SubsystemBase {
     static double m_count = 0;
     double m_prevCount = 0;
     Counter counter;
     Spark gripperTalon;
-    Spark armExtendNeo;
-    Spark armRotateNeo;
+    CANSparkMax armExtendNeo;
+    RelativeEncoder extendEncoder;
+    SparkMaxPIDController extendControl;
+    CANSparkMax armRotateNeo;
+    RelativeEncoder rotateEncoder;
+    SparkMaxPIDController rotateControl;
+
+
+
+
+    double[] rotateAngles = {};
+    double[] extendDistances = {};
+
+
+
     public void ArmSubsystem() {
         counter = new Counter(4);
         gripperTalon = new Spark(0);
-        armExtendNeo = new Spark(0);
-        armRotateNeo = new Spark(0);
+        armExtendNeo = new CANSparkMax(30, MotorType.kBrushless);
+        extendEncoder = armExtendNeo.getEncoder();
+        extendControl = armExtendNeo.getPIDController();
+        extendControl.setP(1);
+        extendControl.setI(0);
+        extendControl.setD(0);
+        extendControl.setIZone(0);
+        extendControl.setFF(0);
+        armRotateNeo = new CANSparkMax(31, MotorType.kBrushless);
+        rotateEncoder = armRotateNeo.getEncoder();
+        rotateControl = armRotateNeo.getPIDController();
+        rotateControl.setP(1);
+        rotateControl.setI(0);
+        rotateControl.setD(0);
+        rotateControl.setIZone(0);
+        rotateControl.setFF(0);
         counter.reset();
+    
  // Set up the input channel for the counter
   //counter.setUpSource(4);bbb
 
@@ -70,20 +108,59 @@ public class ArmSubsystem extends SubsystemBase {
         }
     }
 
-    public void setExtension (int location) {
+    public boolean setExtend (int location) {
         if (location > armExtendNeo.get()) {
             armExtendNeo.set(1);
         } else if (location < armExtendNeo.get()) {
             armExtendNeo.set(-1);
+        } else if (location == armExtendNeo.get()) {
+            return false;
         }
+        return true;
     }
 
-    public void setRotation (int location) {
+    public boolean setRotation (int location) {
         if (location > armRotateNeo.get()) {
             armRotateNeo.set(1);
         } else if (location < armRotateNeo.get()) {
             armRotateNeo.set(-1);
+        } else if (location == armRotateNeo.get()) {
+            return false;
         }
+        return true;
     }
+
+    public void runPresetTable (int[] extendList, int[] rotateList) {
+        for (int i = 0;i < extendList.length; i++) {
+            boolean rotateDone = false;
+            boolean extendDone = false;
+            if (rotateDone && extendDone != true) {
+            if (rotateList[i] != armRotateNeo.get()) {
+                setRotation(rotateList[i]);
+            } else {
+                rotateDone = true;
+            }
+            if (extendList[i] != armExtendNeo.get()) {
+                setExtend(extendList[i]);
+            } else {
+                rotateDone = true;
+            }
+        }
+        }
+
+        boolean frameSatisfied = false;
+        //if (frameSatisfied != true)
+    }
+    public void setArmAngle(double angle) {
+        rotateControl.setReference(angle, ControlType.kPosition);
+        int i = 0;
+        while(angle > rotateAngles[i]) {
+            i++;
+        }
+
+        double extend = extendDistances[i-1] + ((angle - rotateAngles[i-1]) / (rotateAngles[i] - rotateAngles[i-1])) * (extendDistances[i] - extendDistances[i-1]);
+        extendControl.setReference(extend, ControlType.kPosition);
+    }
+
     
 }
