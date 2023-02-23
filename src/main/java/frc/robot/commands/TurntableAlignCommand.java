dbb85ec4078248127fbb5cf8787bcdca2c30e4b7
TheFlippedSuit <111720831+TheFlippedSuit@users.noreply.github.com>
2023-02-22 18:39:18 -0600

    Seperated the Vision Subsytem into methods

--- src/main/java/frc/robot/commands/TurntableAlignCommand.java
+++ src/main/java/frc/robot/commands/TurntableAlignCommand.java
@@ -2,7 +2,6 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.TurntableSubsystem;
-import frc.robot.subsystems.OpenCVVisionSubsystem;
 
 public class TurntableAlignCommand extends CommandBase{
     TurntableSubsystem m_subsystem;
