8aa8053547fb219313647d1f4ee56d7daef24ba9
steohlson <steohlson@gmail.com>
2023-02-24 11:27:50 -0600

    Temp Save 1

---
+++ src/main/java/frc/robot/commands/ArmRetractCommand.java
@@ -0,0 +1,37 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.ArmSubsystem;
+
+public class ArmRetractCommand extends CommandBase {
+    ArmSubsystem m_subsystem;
+
+    public ArmRetractCommand(ArmSubsystem subsystem) {
+        m_subsystem = subsystem;
+        // Use addRequirements() here to declare subsystem dependencies.
+        addRequirements(subsystem);
+    }
+
+
+
+   @Override
+    public void initialize() {
+        m_subsystem.setExtendPosition(-0.1);
+    }
+
+    @Override
+    public void execute() {
+        m_subsystem.setExtendPosition(-0.1);
+
+        
+        
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        m_subsystem.setExtendSpeed(0);
+
+        //m_subsystem.setArmAngle(m_subsystem.rotateEncoder.getPosition());
+    }
+}
